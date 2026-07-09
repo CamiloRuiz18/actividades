bb129d125803fe6c459ef9bf4c444651a63a80af
Camilo Andres Ruiz Sanchez <andresruiz@MacBook-Air-de-Camilo.local>
2026-07-09 02:34:29 -0500

    corregimos la estructura que teniamos de las funciones y logramos dar resultado a la operacion de suma

--- Calculadora2.0.js
+++ Calculadora2.0.js
@@ -2,22 +2,32 @@ const pantalla = document.getElementById("pantalla");
 
 const botones = document.querySelectorAll(".numeros");
 const operadores = document.querySelectorAll(".operador")
+let operacion = ""
+let num1 = ""
+let num2 = ""
+let respuesta = ""
 
 botones.forEach(function (boton) {
 
     boton.addEventListener("click", function () {
         pantalla.value = pantalla.value + boton.textContent;
-
-        operadores.forEach(function (operador) {
-
-            operador.addEventListener("click", function () {
-
-                pantalla.value = ""
-                pantalla.value = "hola"
-            })
-
-        });
-
     });
+});
 
+operadores.forEach(function (operador) {
+
+    operador.addEventListener("click", function () {
+        num1 = parseFloat(pantalla.value)
+        operacion = operador.textContent
+        pantalla.value = ""
+    })
+
+});
+
+document.getElementById("igual").addEventListener("click", function (){
+    num2 = parseFloat(pantalla.value)
+    if(operacion === "+"){
+        respuesta = num1 + num2
+        pantalla.value = parseFloat(respuesta)
+    }
 });
