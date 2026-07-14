0911ddb218e5134dd3b66dbbcb279074608052e3
Camilo Andres Ruiz Sanchez <andresruiz@MacBook-Air-de-Camilo.local>
2026-07-14 09:26:59 -0500

    Finalizamos el convertidor

--- convertidor.js
+++ convertidor.js
@@ -1,17 +1,15 @@
 let Numero1 = ""
 let Convertir =""
-let Centigrados = 0
+let respuesta = 0
 
 document.getElementById("igual").addEventListener("click", function(){
     Numero1 = Num1.value
     Convertir = grados.value
     if (Convertir === "°C"){
-        Centigrados = Numero1 * 1.8 + 32
-        console.log(Centigrados)
+        respuesta = Numero1 * 1.8 + 32;
+        resultado.textContent = "El resultado es: " + respuesta;
     } else {
-        console.log(Numero1)
-        console.log(Centigrados)
-        Centigrados = (Numero1 - 32) / 1.8
-        console.log(Centigrados)
+        respuesta = (Numero1 - 32) / 1.8;
+        resultado.textContent = "El resultado es: " + respuesta;
     }
 })
