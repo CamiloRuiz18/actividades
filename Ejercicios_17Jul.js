3b16a3d45c91cf605e13fa305954cdcab15bdb97
Camilo Andres Ruiz Sanchez <andresruiz@MacBook-Air-de-Camilo.local>
2026-07-15 09:07:03 -0500

    creamos archivos para trabajar

---
+++ Ejercicios_17Jul.js
@@ -0,0 +1,44 @@
+/*1. Escriba un programa que verifique si un número es positivo, negativo o cero e
+imprima el resultado.*/
+
+String = "DIGITE UN VALOR NUMERICO"
+console.log(String)
+prompt
+
+
+
+/*2. Cree un programa que determine si una persona es menor de edad, adulta o
+anciana según su edad, debe pedir el nombre e imprimir nombre y la categoría
+(ejemplo: “Mauricio eres adulto”).*/
+
+
+
+
+/*3. Utilice un bucle for para imprimir los valores de i desde 0 hasta 4.*/
+
+
+
+
+/*4. Utilice un bucle while para imprimir los valores de un contador desde 0 hasta 4.*/
+
+
+
+
+/*5. Cree un bucle for que se detenga cuando i sea igual a 3 usando la instrucción
+break.*/
+
+
+
+
+/*6. Implemente un bucle for que salte la iteración cuando j sea igual a 2 usando la
+instrucción continue.*/
+
+
+
+
+/*7. Convierta un número del 1 al 12 en el nombre del mes correspondiente. El
+programa debe solicitar al usuario que ingrese un número del 1 al 12 y luego
+mostrar el nombre del mes asociado.
+Ejemplo:
+• Ingresa un número del 1 al 12 para saber el mes correspondiente: 9
+• El mes correspondiente al número 9 es: Septiembre*/
