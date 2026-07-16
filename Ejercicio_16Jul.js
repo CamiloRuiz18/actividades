df2ca64aa7fc88123e69871b9ea6b153830e89e9
Camilo Andres Ruiz Sanchez <andresruiz@MacBook-Air-de-Camilo.local>
2026-07-16 17:04:47 -0500

    tercer ejercicio resuelto

--- Ejercicio_16Jul.js
+++ Ejercicio_16Jul.js
@@ -6,7 +6,7 @@ let Area = 0;
 let Perimetro = 0;
 let datos = {}
 
-function operaciones(){
+function operaciones() {
 
     Radio = parseFloat(document.getElementById("Radio").value);
     Area = Math.PI * (Radio ** 2) + " Cm2"
@@ -15,7 +15,7 @@ function operaciones(){
     datos.Area = Area
     datos.Perimetro = Perimetro
 
-    document.getElementById("Respuesta").innerHTML = "Radio: " + Radio+"cm" + "\n" + "Area: " + datos.Area + "\n" + "Perimetro: " + datos.Perimetro
+    document.getElementById("Respuesta").innerHTML = "Radio: " + Radio + "cm" + "\n" + "Area: " + datos.Area + "\n" + "Perimetro: " + datos.Perimetro
 
 }
 document.getElementById("Calcular").addEventListener("click", operaciones)
@@ -25,13 +25,13 @@ document.getElementById("Calcular").addEventListener("click", operaciones)
 parámetros: base y exponente. Esta función debe calcular y devolver el resultado
 de elevar la base al exponente.*/
 
-function calcualar (){
+function calcualar() {
 
     let Base = parseFloat(document.getElementById("Base").value)
     let Exponente = parseFloat(document.getElementById("Exponente").value)
-    
+
     let Respuesta = Base ** Exponente
-    document.getElementById("Respuesta2").innerHTML = "La respuesta es: " + Respuesta 
+    document.getElementById("Respuesta2").innerHTML = "La respuesta es: " + Respuesta
 }
 document.getElementById("Calcular2").addEventListener("click", calcualar)
 
@@ -39,6 +39,31 @@ document.getElementById("Calcular2").addEventListener("click", calcualar)
 
 /*3. Cree un programa que solicite al usuario ingresar una lista de números y luego
 encuentre el número más grande en esa lista.*/
+
+let numeroMayor = 0;
+let comparar = 0;
+let contador = 0;
+
+document.getElementById("ingresar").addEventListener("click", function () {
+
+    comparar = parseFloat(document.getElementById("list").value)
+
+    if (comparar > numeroMayor) {
+        numeroMayor = comparar
+    }
+
+    contador++;
+
+     if (contador === 10) {
+
+        document.getElementById("Respuesta3").innerHTML = "El numero mayor de la lista es: " + numeroMayor
+
+        contador = 0;
+        numeroMayor = 0;
+        document.getElementById("list").value = "";
+    }
+
+})
 
 
 
