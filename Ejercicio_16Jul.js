3c130ae3139c14fa587974041ad9a23edc00d5b3
Camilo Andres Ruiz Sanchez <andresruiz@MacBook-Air-de-Camilo.local>
2026-07-16 10:00:40 -0500

    Creamos los archivos para trabajar

---
+++ Ejercicio_16Jul.js
@@ -0,0 +1,29 @@
+/*1. Escriba un programa que solicite al usuario el radio de un círculo y luego calcule
+y muestre el área y el perímetro del círculo, debe utilizar funciones.*/
+
+
+
+
+/*2. Escriba un programa que utilice una función llamada calcular la cual toma dos
+parámetros: base y exponente. Esta función debe calcular y devolver el resultado
+de elevar la base al exponente.*/
+
+
+
+
+/*3. Cree un programa que solicite al usuario ingresar una lista de números y luego
+encuentre el número más grande en esa lista.*/
+
+
+
+
+/*4. utilizando el Bucle for haga:
+• Imprimir los números del 20 al 30 en la consola.
+• Imprimir los números pares del 30 al 50 en la consola.
+• Calcular la suma de los primeros 50 números naturales.
+• Imprimir la tabla de multiplicar del 8.
+• Imprimir los elementos de un array.
+• Imprimir un patrón de asteriscos en forma de triángulo, con altura 9.
+• Calcular la suma de los números pares del 1 al 50.
+• Imprimir los números del 30 al 20 en orden descendente.
+• Calcular el promedio de un array de números.*/
