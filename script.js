3ee6a08a52160cb500cc77b85167a93b6be848de
Camilo Andres Ruiz Sanchez <andresruiz@MacBook-Air-de-Camilo.local>
2026-07-28 07:42:56 -0500

    filtros modificados, falta realizar las funciones

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+ /*filtros*/
+const categorias = document.querySelectorAll(".contenedorFiltros > div");
+
+categorias.forEach(categoria => {
+
+    const titulo = categoria.querySelector(".cajon");
+    const filtros = categoria.querySelector(".filtros");
+    const flecha = categoria.querySelector(".cajon");
+
+    titulo.addEventListener("click", () => {
+        filtros.classList.toggle("oculto");
+        flecha.classList.toggle("rotar");
+    });
+
+});
