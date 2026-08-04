a2913cae58de4d65d1619fa77f052916cb7ebe6b
Camilo Andres Ruiz Sanchez <andresruiz@MacBook-Air-de-Camilo.local>
2026-08-04 10:07:10 -0500

    Inicio

--- tiendaNike/demo/src/main/java/com/nike/demo/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nike.demo.repository;
-
-import com.nike.demo.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-
-}
