ebf6f0676f092f0e1156831d1d86a12575636548
Camilo Andres Ruiz Sanchez <andresruiz@MacBook-Air-de-Camilo.local>
2026-08-04 09:56:12 -0500

    Separamos y conectamos con mysql

---
+++ Backend/conexion.js
@@ -0,0 +1,17 @@
+//conexion con la base de datos
+const mysql = require("mysql2");
+
+const conexion = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Camiloruiz123@",
+    database:"Nike"
+})
+// Prueba la conexión
+conexion.connect((error) => {
+  if (error) {
+    console.error('Error al conectar a la base de datos:', error.stack);
+    return;
+  }
+  console.log('Conexión exitosa' + conexion.threadId + '');
+});
